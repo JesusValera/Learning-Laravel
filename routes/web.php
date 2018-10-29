957fb532da155dc082830f26940aabc15bf4d226
Jesus Valera <jesus1902@outlook.com>
2018-10-29 20:30:45 +0100

    Routes ->routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/users', function () {
+    return '<body>Hello users!</body>';
+});
+
+Route::get('users/new', function () {
+    return 'Creating new user.';
+});
+
+Route::get('/users/{id}', function ($id) {
+    return "Showing details of user with id $id";
+})->where('id', '\d+');
+
+Route::get('/users/{name}/{nickname?}', function ($name, $nickname = null) {
+    if (!isset($nickname))
+        return "Hello $name without nickname.";
+
+    return "Hello $name whose nickname is $nickname";
+});
+
